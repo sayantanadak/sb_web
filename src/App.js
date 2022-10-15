1ac832e0f32aae37b1c4587d6674bf7a4bf8988d
sayantan_adak <sayantan.adak96@gmail.com>
2022-10-15 13:21:23 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="App">
+        <img src={require("./assets/indicator.gif")} alt="logo" />
+      </header>
+      <header className="App">
+        <h1 className="alert-title">Site is under Development</h1>
       </header>
     </div>
   );
